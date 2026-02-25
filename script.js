349a07b0593fbd62e6c9625f3d2d20b0509956fd
Rishav Prajapati <rishavp063@gmail.com>
2026-02-25 23:08:26 +0530

    code modified

--- script.js
+++ script.js
@@ -1,20 +1,32 @@
 let darkMode = document.querySelector(".toggle-on");
 let lightMode = document.querySelector(".toggle-off");
 let center = document.querySelector(".center");
+let footer = document.querySelector(".footer");
+let logo = document.querySelector(".header .logo");
+let menu = document.querySelector(".lists");
 
 let check = 1;
 
 lightMode.addEventListener("click", () => {
   if (check == 1) {
-    center.style.color = "white";
-    center.style.backgroundColor = "black";
+    document.body.style.color = "white";
+    document.body.style.backgroundColor = "black";
+    footer.style.color = "white";
+    logo.style.color = "white";
+    menu.style.color = "white";
+    darkMode.style.display = "block";
     check = 0;
-  } else {
-    darkMode.addEventListener("click", () => {
-      darkMode.style.visibility = "visible";
-      document.body.style.color = "black";
-      document.body.style.backgroundColor = "white";
-      check = 1;
-    });
+  }
+});
+
+darkMode.addEventListener("click", () => {
+  if (check == 0) {
+    document.body.style.color = "black";
+    document.body.style.backgroundColor = "white";
+    footer.style.color = "black";
+    logo.style.color = "black";
+    menu.style.color = "black";
+    darkMode.style.display = "none";
+    check = 1;
   }
 });
