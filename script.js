8d1b474ba2bfd82765aa578f51cf14aadca0d84f
Rishav Prajapati <rishavp063@gmail.com>
2026-03-08 16:11:23 +0530

    code modified

--- script.js
+++ script.js
@@ -1,32 +1,25 @@
-let darkMode = document.querySelector(".toggle-on");
-let lightMode = document.querySelector(".toggle-off");
-let center = document.querySelector(".center");
-let footer = document.querySelector(".footer");
-let logo = document.querySelector(".header .logo");
-let menu = document.querySelector(".lists");
+let toggleOn = document.querySelector("#toggle-on");
+let toggleOff = document.querySelector("#toggle-off");
+let togglelBtn = document.querySelector(".toggle-btn");
 
 let check = 1;
 
-lightMode.addEventListener("click", () => {
-  if (check == 1) {
-    document.body.style.color = "white";
-    document.body.style.backgroundColor = "black";
-    footer.style.color = "white";
-    logo.style.color = "white";
-    menu.style.color = "white";
-    darkMode.style.display = "block";
-    check = 0;
-  }
+const enableDarkmode = () => {
+  document.body.classList.add("darkmode");
+  toggleOn.style.display = "inline-block";
+  toggleOff.style.display = "none";
+};
+
+const disableDarkmode = () => {
+  document.body.classList.remove("darkmode");
+  toggleOff.style.display = "inline-block";
+  toggleOn.style.display = "none";
+};
+
+togglelBtn.addEventListener("click", () => {
+  check === 1 ? enableDarkmode() : disableDarkmode();
+  console.log("hello");
 });
 
-darkMode.addEventListener("click", () => {
-  if (check == 0) {
-    document.body.style.color = "black";
-    document.body.style.backgroundColor = "white";
-    footer.style.color = "black";
-    logo.style.color = "black";
-    menu.style.color = "black";
-    darkMode.style.display = "none";
-    check = 1;
-  }
-});
+toggleOn.addEventListener("click", disableDarkmode);
+toggleOff.addEventListener("click", enableDarkmode);
